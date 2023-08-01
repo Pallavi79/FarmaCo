6f43b9905a429c4820bd7d4fc77053b467ffa9ea
Pallavi <pallavi3749@gmail.com>
2023-08-01 18:05:12 +0530

    front end changes

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+    # return render_template("index.html")
 
 @app.route("/predict",methods=['POST'])
 def predict():
